c5a7b4ef7c7e37b33260f9405a8fdc7831cf7245
JCelayaRdz <jcelayardz@gmail.com>
2024-05-24 19:34:07 +0200

    add bean to populate the database with sample data

---
+++ src/main/java/com/jcelayardz/library_rest_api/config/SqlPopulator.java
@@ -0,0 +1,26 @@
+package com.jcelayardz.library_rest_api.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SqlPopulator {
+
+    @Bean
+    public CommandLineRunner dataInitialization(DataSource dataSource) {
+        return args -> {
+            var populator = new ResourceDatabasePopulator();
+            populator.addScripts(
+                    new ClassPathResource("schema.sql"),
+                    new ClassPathResource("sample-data.sql")
+            );
+            DatabasePopulatorUtils.execute(populator, dataSource);
+        };
+    }
+}
